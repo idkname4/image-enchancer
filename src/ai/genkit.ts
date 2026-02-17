2e90ebe2e9ad57eeebca3ccac3dee42ba94202b6
idk_name123 <idkname486@gmail.com>
2026-02-17 16:57:40 +0000

    it looks like mt aoi krey is been publick;ly exploit expose so i want t

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,11 +3,11 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({apiKey:"AIzaSyA-8jT4KOuJ88bn2fI6sMTU1OJxBN7dIkA"
+    googleAI({
       // Add your API key here. For example:
       // apiKey: "YOUR_API_KEY"
       // or from an environment variable (recommended):
-      // apiKey: process.env.GEMINI_API_KEY
+      apiKey: process.env.GEMINI_API_KEY
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
